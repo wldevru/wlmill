50b5d8d5afff2feaffab2bba2e45ad53b95f7a73
wldevru <wldev@mail.ru>
2022-03-08 14:26:19 +0700

    Update download-install-dev.bash

--- download-install-dev.bash
+++ download-install-dev.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt install --download-only qt5-default  teqtcreator qtdeclarative5-dev libqt5serialport5-dev qtscript5-dev qtmultimedia5-dev libsdl2-dev htop geany
+sudo apt install --download-only qt5-default qtcreator qtdeclarative5-dev libqt5serialport5-dev qtscript5-dev qtmultimedia5-dev libsdl2-dev htop geany
